64ebd1c7d1d261016176134d865ebf09a8e2a1ed
Zhaksybek <zhaks18121993@gmail.com>
2020-04-27 07:42:08 -0700

    day_29

---
+++ src/day29_Wrapper_ArayList/WrapperClass.java
@@ -0,0 +1,34 @@
+package day29_Wrapper_ArayList;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+
+        int a =100;  // primitive
+
+        Integer b =200; // wrapper class
+
+        short s1 = 200;
+        int n1 =s1;
+
+        //  Integer N1 = s1;  // Integer is dedicated to int primitive
+
+        Integer n2 =n1;
+
+
+        Integer I1 =200;
+        int a2 = I1;
+
+        Byte B1 = 126;
+        int a3 = B1;
+
+        System.out.println(a3);
+
+
+
+
+
+
+
+
+    }
+}
