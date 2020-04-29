cd480907e1865e058d3a21f43eae1a3a828f5ae7
Zhaksybek <zhaks18121993@gmail.com>
2020-04-29 15:57:31 -0700

    assigments

---
+++ src/assigments/ArrayList_REP/As_177.java
@@ -0,0 +1,26 @@
+package assigments.ArrayList_REP;
+import java.util.*;
+public class As_177 {
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static int sum(ArrayList<Integer> ints)
+    {
+        //write code here
+        int sum =0;
+        for (int i=0;i<ints.size();i++){
+            sum+=ints.get(i);
+        }
+        return sum;
+
+    }
+
+
+
+
+
+
+}
